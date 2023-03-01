2263cb9084bdcb7e10dc4afc41685d99b720fd40
Chris Adams <cfh1990@msn.com>
2023-03-01 19:39:34 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python Cookbook Recipes
+
+Useful recipes from the O'Reilly book [Python Cookbook (3rd edition)](https://www.oreilly.com/library/view/python-cookbook-3rd/9781449357337/).
---
+++ src/01_data_structures_and_algorithms/03_keep_last_n_items/example.py
@@ -0,0 +1,23 @@
+"""
+You want to keep limited history of the last few items seen during iteration 
+or during some kind of processing.
+"""
+
+from collections import deque
+from pathlib import Path
+
+def search(lines, pattern, history=5):
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines
+        previous_lines.append(line)
+        
+if __name__ == '__main__':
+    
+    with open(Path(__file__).parent / 'somefile.txt', 'r') as f:
+        for line, previous_lines in search(f, 'python', 5):
+            for prev_line in previous_lines:
+                print(prev_line, end='')
+            print(line, end='')
+            print('-' * 20)
