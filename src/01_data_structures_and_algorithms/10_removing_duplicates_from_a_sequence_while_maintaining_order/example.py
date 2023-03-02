687412468280b95f11c5c409464a25f9fa7aa6d8
Chris Adams <cfh1990@msn.com>
2023-03-02 21:52:07 +0000

    updated recipes

--- src/01_data_structures_and_algorithms/10_removing_duplicates_from_a_sequence_while_maintaining_order/example.py
+++ src/01_data_structures_and_algorithms/10_removing_duplicates_from_a_sequence_while_maintaining_order/example.py
@@ -21,8 +21,16 @@ def dedupe_unhashable(items, key):
 if __name__ == '__main__':
     a = [1, 5, 2, 1, 9, 1, 5, 10]
     b = [{'x': 1, 'y': 2}, {'x': 1, 'y': 3}, {'x': 1, 'y': 2}, {'x': 2, 'y': 4}]
-    print(f'De-duping {a} (hashable)')
+
+    print(f'\nDe-duping {a} (hashable)')
     print(list(dedupe_hashable(a)))
     print('=' * 50)
-    print(f'De-duping {b} (unhashable)')
+
+    print(f'\nDe-duping {b} (unhashable)')
+    print(' (de-duping on the x and y key values)')
     print(list(dedupe_unhashable(b, key=lambda d: (d['x'], d['y']))))
+    print('=' * 50)
+
+    print(f'\nDe-duping {b} (unhashable)')
+    print(' (de-duping on the x key value)')
+    print(list(dedupe_unhashable(b, key=lambda d: d['x'])), end='\n\n')
