798c1ec21cc298d4d827323334b8b7464df84b80
Chris Adams <cfh1990@msn.com>
2023-03-04 12:57:32 +0000

    refactored __main__

--- recipes/__main__.py
+++ recipes/__main__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from argparse import ArgumentParser
 from collections import defaultdict
 from importlib import import_module
-
+from recipes import Recipe
 
 ROOT = Path(__file__).parent
 
@@ -25,75 +25,45 @@ def parse_args(argv):
 def get_recipes():
     result = defaultdict(list)
 
-    for chapter in sorted(ROOT.iterdir()):
-        if chapter.stem[0].isdigit():
-            for recipe in sorted(chapter.iterdir()):
-                result[chapter.stem].append(recipe.stem)
+    for recipe_path in sorted(ROOT.glob('**/example.py')):
+        recipe = Recipe(recipe_path)
+        result[recipe.chapter].append(recipe)
     return result
-
-
-def clean_text(text):
-    num, text = text.split('_', maxsplit=1)
-    return f"{int(num):>2}) {text.replace('_', ' ').capitalize()}"
 
 
 def render_recipes(recipes):
     for chapter, recipes in recipes.items():
-        print(clean_text(chapter).title())
+        print(chapter)
         print('=' * 50)
         for recipe in recipes:
-            print(f'  {clean_text(recipe)}')
+            print(f'  {recipe.example}')
         print()
 
 
-def find_recipe(args):
-    result = None
-    chapter = f'{args.chapter:0>2}'
-    number = f'{args.number:0>2}'
-    
-    chapter_dir = [x for x in ROOT.iterdir() if x.is_dir() and x.stem.startswith(chapter)]
-    if chapter_dir:
-        recipe_dir = [x for x in chapter_dir[0].iterdir() if x.is_dir() and x.stem.startswith(number)]
-        if recipe_dir:
-            return recipe_dir[0] / 'example.py'
-    return result
-
-
-def recipe_handler(recipe, args):
-    if recipe is None:
-        print('No recipe found')
-    else:
-        module = get_recipe_module(recipe)
-        print(f'Found recipe:\n{clean_text(recipe.parent.stem)}')
-        print(module.__doc__)
-
-        if args.code:
-            print(recipe.read_text())
-            sys.exit(0)
-
-        if args.doc:
-            sys.exit(0)
-
-        run_recipe(recipe)
-
-
-def get_recipe_module(recipe):
-    chapter = recipe.parent.parent.stem
-    submodule = recipe.parent.stem
-    return import_module(f'recipes.{chapter}.{submodule}.example')
-
-
-def run_recipe(recipe):
-    module = get_recipe_module(recipe)
-    func = getattr(module, 'main')
-    print('Running...')
-    func()
+def find_recipe_path(args):
+    try:
+        return next(ROOT.glob(f'**/{args.chapter:0>2}*/{args.number:0>2}*/example.py'))
+    except StopIteration:
+        print('Recipe not found')
+        sys.exit(0)
 
 
 def main(argv):
     args = parse_args(argv)
-    recipe = find_recipe(args)
-    recipe_handler(recipe, args)
+    recipe = Recipe(find_recipe_path(args))
+
+    print(f'Found recipe:\n{recipe}')
+    print(recipe.get_docstring())
+
+    if args.doc:
+        sys.exit(0)
+
+    if args.code:
+        print(recipe.get_code())
+        sys.exit(0)
+
+    recipe.run()
+    sys.exit(0)
 
 
 if __name__ == '__main__':
