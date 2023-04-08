c91ad508942c8e3f93fb4a45664ff2fd35d8c847
Chris Adams <cfh1990@msn.com>
2023-04-08 19:09:27 +0100

    updates tests - adds recipes 7.2 and 7.3

--- tests/test_recipe.py
+++ tests/test_recipe.py
@@ -3,11 +3,19 @@ from pathlib import Path
 from recipes import Recipe
 
 
-def test_recipe__valid_recipe_module_instantiates(recipe_path):
+def test_recipe__valid__doesnt_exist__recipe_module_instantiates(capsys):
     recipe = Recipe(Path('./01_testing_chapter/02_testing_example/example.py'))
+    recipe.run()
+    out, err = capsys.readouterr()
+
     assert recipe.module == 'example'
     assert recipe.chapter == ' 1) Testing Chapter'
     assert recipe.example == ' 2) Testing example'
+    assert recipe.package == ''
+    assert 'Couldn\'t find recipe  2) Testing example' in out
+    assert err == ''
+    with pytest.raises(ModuleNotFoundError, match='This recipe couldn\'t be found:'):
+        recipe.get_module()
 
 
 def test_recipe__invalid_recipe_module_raises_ValueError():
@@ -20,5 +28,7 @@ def test_recipes_running(recipe_path, capsys):
     recipe.run()
 
     out, err = capsys.readouterr()
+    assert 'TODO' not in recipe.get_docstring()
+    assert "if __name__ == '__main__':" in recipe.get_code()
     assert 'Running...' in out
     assert err == ''
