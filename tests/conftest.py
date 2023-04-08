ea281b9a2bea47d00022ccc8b62c3fce48f8ae28
Chris Adams <cfh1990@msn.com>
2023-04-08 20:27:35 +0100

    updates tests / fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,26 @@
 import pytest
-from recipes import ROOT
+from recipes import ROOT, __main__
+
+
+TEMPLATE = (ROOT / 'template.py').read_text()
 
 
 @pytest.fixture(params=list(ROOT.glob('**/example.py')))
 def recipe_path(request):
     yield request.param
+
+
+@pytest.fixture
+def recipe_root_dir(tmp_path_factory):
+    root_dir = tmp_path_factory.mktemp('recipes')
+    recipe_dir = root_dir / '01_test_chapter' / '01_test_recipe'
+    recipe_dir.mkdir(parents=True, exist_ok=True)
+    (recipe_dir / 'example.py').write_text(TEMPLATE)
+    (root_dir / 'template.py').write_text(TEMPLATE)
+    yield root_dir
+
+
+@pytest.fixture
+def patched_root(recipe_root_dir, monkeypatch):
+    monkeypatch.setattr(__main__, 'ROOT', recipe_root_dir)
+    yield monkeypatch
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,12 @@
 import pytest
-from recipes.__main__ import main
+from recipes import __main__ as main
 from recipes import template
+from tests.conftest import TEMPLATE
 
 
 def test_main_with_no_args_lists_recipes(capsys):
     with pytest.raises(SystemExit) as exc:
-        main([])
+        main.main([])
     out, err = capsys.readouterr()
     assert exc.value.code == 0
     assert err == ''
@@ -16,7 +17,7 @@ def test_main_with_no_args_lists_recipes(capsys):
 @pytest.mark.parametrize('argv', [['1', '5'], ['1', '5', '--code'], ['1', '5', '--doc']])
 def test_main__recipe_exists(argv, capsys):
     with pytest.raises(SystemExit) as exc:
-        main(argv)
+        main.main(argv)
     out, err = capsys.readouterr()
     assert exc.value.code == 0
     assert err == ''
@@ -25,7 +26,7 @@ def test_main__recipe_exists(argv, capsys):
 
 def test_main__recipe_doesnt_exist(capsys):
     with pytest.raises(SystemExit) as exc:
-        main(['100', '20'])
+        main.main(['100', '20'])
     out, err = capsys.readouterr()
     assert exc.value.code == 0
     assert err == ''
@@ -34,3 +35,40 @@ def test_main__recipe_doesnt_exist(capsys):
 
 def test_template():
     assert template.main() is None
+
+
+def test_recipes__create__generates_template(recipe_root_dir, capsys, patched_root):
+
+    with pytest.raises(SystemExit) as exc:
+        main.main(['1', '2', '--create', 'new test recipe'])
+    out, err = capsys.readouterr()
+    expected_output_path = recipe_root_dir / '01_test_chapter' / '02_new_test_recipe' / 'example.py'
+
+    assert exc.value.code == 0
+    assert err == ''
+    assert 'created recipe:' in out
+    assert expected_output_path.exists()
+    assert expected_output_path.read_text() == TEMPLATE
+
+
+def test_find_chapter_dir__exists(recipe_root_dir, patched_root):
+    chapter = main.find_chapter_dir(1)
+    assert chapter == recipe_root_dir / '01_test_chapter'
+
+
+def test_find_chapter_dir__not_exists(recipe_root_dir, capsys, patched_root):
+    with pytest.raises(SystemExit) as exc:
+        main.find_chapter_dir(42)
+    out, err = capsys.readouterr()
+
+    assert exc.value.code == 0
+    assert out == 'Chapter 42 couldn\'t be found\n'
+    assert err == ''
+
+
+def test_render_recipes__no_recipes_found(capsys):
+    result = main.render_recipes([])
+    out, err = capsys.readouterr()
+    assert result is None
+    assert err == ''
+    assert out == 'No recipes found\n'
