925fd4c315e1337d435da29702a55dcae70c034b
Chris Adams <cfh1990@msn.com>
2023-06-06 22:03:26 +0100

    adds more recipes

--- recipes/08_classes_and_objects/06_creating_managed_attributes/example.py
+++ recipes/08_classes_and_objects/06_creating_managed_attributes/example.py
@@ -8,7 +8,7 @@ import math
 
 class Person:
     def __init__(self, first_name):
-        self._first_name = first_name
+        self.first_name = first_name
 
     @property
     def first_name(self):
