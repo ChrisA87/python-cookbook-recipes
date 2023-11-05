9dfb18abc26751ddc1d2cb993dbe478afe82ed04
Chris Adams <cfh1990@msn.com>
2023-11-05 15:00:34 +0000

    updates functionality of cli ls subcommand

--- pyrecipes/cli/ls.py
+++ pyrecipes/cli/ls.py
@@ -1,5 +1,16 @@
 import click
+from pyrecipes.chapter import Chapter
 from pyrecipes.cookbook import cookbook
+
+
+def render_chapter(chapter: Chapter, show_description: bool = False):
+    width = len(str(chapter)) + 2
+    click.echo(f'{"=" * width}\n{str(chapter):^{width}}\n{"=" * width}')
+    for _, recipe in chapter:
+        click.echo(f"  {recipe}")
+        if show_description:
+            click.echo(f"    {recipe.get_docstring()}\n")
+    click.echo("")
 
 
 @click.command()
@@ -10,12 +21,12 @@ from pyrecipes.cookbook import cookbook
     help="List the recipes from a specific chapter",
     multiple=True,
 )
-def ls(chapter):
+@click.option("-s", "--show-description", is_flag=True)
+def ls(chapter, show_description):
     """List recipes"""
     if chapter:
-        for chpt in sorted(chapter):
-            click.echo(f"listing all recipes from chapter {chpt}")
-            click.echo(cookbook.get_recipes_by_chapter(chpt))
+        for c in sorted(chapter):
+            render_chapter(cookbook[c], show_description)
     else:
-        click.echo("listing all recipes")
-        click.echo(cookbook)
+        for _, chapter in cookbook:
+            render_chapter((chapter), show_description)
