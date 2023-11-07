98a97f50df1bf100371d4985ace98792f954f560
Chris Adams <cfh1990@msn.com>
2023-11-07 21:52:20 +0000

    updates exception handling in cli commands

--- pyrecipes/cli/show.py
+++ pyrecipes/cli/show.py
@@ -1,6 +1,6 @@
 import click
 from pyrecipes.cookbook import cookbook
-from pyrecipes.recipe import Recipe
+from pyrecipes.errors import ChapterNotFoundError, RecipeNotFoundError
 
 
 @click.command()
@@ -8,7 +8,8 @@ from pyrecipes.recipe import Recipe
 @click.argument("number", type=int)
 def show(chapter, number):
     """Shows a recipe"""
-    click.echo(f"Showing recipe {chapter}.{number}")
-    recipe = cookbook[chapter][number]
-    if isinstance(recipe, Recipe):
-        click.echo(recipe.get_code())
+    try:
+        click.echo(f"Showing recipe {chapter}.{number}")
+        click.echo(cookbook[chapter][number].get_code())
+    except (ChapterNotFoundError, RecipeNotFoundError) as exc:
+        click.echo(exc)
