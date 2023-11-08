2f7c89cca9a27fc338a269232d0363bf5884e5d3
Chris Adams <cfh1990@msn.com>
2023-11-08 20:27:16 +0000

    improved search command output

--- pyrecipes/cli/search.py
+++ pyrecipes/cli/search.py
@@ -1,8 +1,11 @@
+from typing import Mapping
 import click
 import re
 from colorama import Fore, init
+from pyrecipes.chapter import Chapter
 from pyrecipes.cookbook import cookbook
 from pyrecipes.recipe import SearchMatch
+from pyrecipes.utils import text_border
 
 init(autoreset=True)
 
@@ -20,14 +23,20 @@ COLOURS = {
 
 
 def render_match(pattern: str, match: SearchMatch, color=Fore.RED):
-    click.echo(
-        f"Recipe: {match.chapter}.{match.recipe_number}, Line: {match.line_number} - {match.recipe_name}"
-    )
-    click.echo(
-        re.sub(
-            re.compile(pattern), color + pattern + Fore.RESET, match.line_text
-        ).lstrip()
-    )
+    text = re.sub(
+        re.compile(pattern), color + pattern + Fore.RESET, match.line_text
+    ).strip()
+    click.echo(f"  Line {match.line_number}: {text}")
+
+
+def render_matches(pattern: str, match_dict: Mapping[Chapter, Mapping]):
+    for chapter, recipes in match_dict.items():
+        click.echo(text_border(str(chapter)))
+        for recipe, matches in recipes.items():
+            click.echo(recipe)
+            for match in matches:
+                render_match(pattern, match)
+            click.echo("")
 
 
 @click.command()
@@ -40,9 +49,8 @@ def render_match(pattern: str, match: SearchMatch, color=Fore.RED):
 )
 def search(pattern, color):
     """Search the recipes for a pattern"""
-    for _, chapter in cookbook:
-        for _, recipe in chapter:
-            matches = recipe.search(pattern)
-            if matches:
-                for match in matches:
-                    render_match(pattern, match, COLOURS.get(color.lower(), "red"))
+    match_dict = cookbook.search(pattern)
+    if match_dict:
+        render_matches(pattern, match_dict)
+    else:
+        click.echo(f"No recipes found matching '{pattern}'")
