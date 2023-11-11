60211066c301d8cb0aa06841889f94f783cbaebd
Chris Adams <cfh1990@msn.com>
2023-11-11 11:38:50 +0000

    Feature docs to search (#20)

--- src/pyrecipes/cli/search.py
+++ src/pyrecipes/cli/search.py
@@ -1,13 +1,8 @@
 import sys
-from typing import Mapping
 import click
-import re
 from colorama import Fore, init
-from colorama.ansi import AnsiFore
-from pyrecipes.chapter import Chapter
 from pyrecipes.cookbook import cookbook
-from pyrecipes.recipe import SearchMatch
-from pyrecipes.utils import text_border
+from .utils import get_n_matches, render_matches
 
 init(autoreset=True)
 
@@ -22,42 +17,6 @@ COLORS = {
     "white": Fore.WHITE,
     "none": Fore.RESET,
 }
-
-
-def get_n_matches(matches):
-    count = 0
-    for recipes in matches.values():
-        for matches in recipes.values():
-            count += sum(match.count for match in matches)
-    return count
-
-
-def render_match(
-    pattern: str, match: SearchMatch, color: AnsiFore, ignore_case: bool = False
-):
-    flags = re.IGNORECASE if ignore_case else 0
-    text = re.sub(
-        pattern,
-        lambda match: f"{color}{match.group()}{Fore.RESET}",
-        match.line_text,
-        flags=flags,
-    ).strip()
-    click.echo(f"  Line {match.line_number}: {text}")
-
-
-def render_matches(
-    pattern: str,
-    match_dict: Mapping[Chapter, Mapping],
-    color: AnsiFore,
-    ignore_case: bool = False,
-):
-    for chapter, recipes in match_dict.items():
-        click.echo(text_border(str(chapter)))
-        for recipe, matches in recipes.items():
-            click.echo(f"{chapter.number}.{recipe}")
-            for match in matches:
-                render_match(pattern, match, color, ignore_case)
-            click.echo("")
 
 
 @click.command()
@@ -78,7 +37,13 @@ def render_matches(
 @click.option(
     "-c", "--count-only", is_flag=True, help="Return the count of matches only."
 )
-def search(pattern, color, ignore_case, count_only):
+@click.option(
+    "-d",
+    "--describe",
+    is_flag=True,
+    help="Shows descriptions of the recipes",
+)
+def search(pattern, color, ignore_case, count_only, describe):
     """Search the recipes for a pattern
 
     \b
@@ -96,4 +61,4 @@ def search(pattern, color, ignore_case, count_only):
         sys.exit(0)
 
     if match_dict:
-        render_matches(pattern, match_dict, color, ignore_case)
+        render_matches(pattern, match_dict, color, ignore_case, describe)
