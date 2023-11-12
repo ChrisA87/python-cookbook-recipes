8cf8840b8547f1f07513a435084194768d695bd2
Chris Adams <cfh1990@msn.com>
2023-11-12 20:20:34 +0000

    updates recipe 5.20 - serial moved to example func

--- src/pyrecipes/recipes/05_files_and_io/20_communicating_with_serial_port/example.py
+++ src/pyrecipes/recipes/05_files_and_io/20_communicating_with_serial_port/example.py
@@ -2,11 +2,15 @@
 You want to read and write data over a serial port, typically to
 interect wit some kind of harware device (e.g. a robot or sensor).
 """
-import serial
 
 
 def example_1():
-    """For illustrative purposes only."""
+    """For illustrative purposes only.
+    Requires pyserial package.
+        `pip install pyserial`
+    """
+    import serial
+
     ser = serial.Serial(
         "/dev/tty.usbmodem641",  # Device name varies
         baudrate=9600,
